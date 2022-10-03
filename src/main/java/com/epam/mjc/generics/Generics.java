360f632222cfd5a9737b1027bb0432e95e40b229
mukhamedovislam <bozuy24@gmail.com>
2022-10-03 12:11:59 +0400

    Refactor Generics Methods

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -20,7 +20,7 @@ public class Generics {
     }
 
     //TODO: Refactor Method-3
-    public void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
+    public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
